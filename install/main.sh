2417fd49e558b82270da8578ab6fac1d5fe2db5b
Mauricio B <m@pull.srl>
2024-08-04 10:53:02 -0400

    install/main:

--- install/main.sh
+++ install/main.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-source ~/macOSInstallation/install/main.sh
+# source ~/macOSInstallation/install/config.sh
+
+
+# Config
+declare gitHubUsername=PULL-SRL
+declare gitHubRepository=macOSInstallation
+
+# Other configuration (don't modify)
+declare cloneFolder=~/${gitHubRepository}
+
 
 # clear
 
@@ -8,6 +17,6 @@ echo ${gitHubUsername}
 echo ${gitHubRepository}
 echo ${cloneFolder}
 
-# source repository/main.sh # clone or update repository
+source repository/main.sh # clone or update repository
 
 # source nav/main.sh # navigation
