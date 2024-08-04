070e02b549bacfe0734341b4e73fc6157c679ce4
M-Pull <156857384+M-Pull@users.noreply.github.com>
2024-08-04 16:07:27 +0000

    install/main:

--- install/main.sh
+++ install/main.sh
@@ -14,6 +14,9 @@ echo ${gitHubRepository}
 echo ${cloneFolder}
 
 echo "curl repooooooooooooooooooo"
+echo "curl repooooooooooooooooooo"
+echo "curl repooooooooooooooooooo"
+echo "curl repooooooooooooooooooo"
 # source ${cloneFolder}/repository/main.sh # clone or update repository
 bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh | bash -s -- ${gitHubUsername} ${gitHubRepository} ${cloneFolder})
 
