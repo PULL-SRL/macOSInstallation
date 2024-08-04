fd534faa0611d0dd72bc16d4af60bd118faf5e9b
M-Pull <156857384+M-Pull@users.noreply.github.com>
2024-08-04 16:07:01 +0000

    repository/main:

--- repository/main.sh
+++ repository/main.sh
@@ -14,7 +14,7 @@ echo "###################"
 if [[ ! -d ${cloneFolder} ]]; then
     echo ""
     echo "Downloading Mac OS Installation..."
-    git clone https://github.com/${gitHubUsername}/${gitHubRepository}.git ${cloneFolder}
+    # git clone https://github.com/${gitHubUsername}/${gitHubRepository}.git ${cloneFolder}
 fi
 
 # Update, repository folder already exists
@@ -22,15 +22,15 @@ if [[ -d ${cloneFolder} ]]; then
     echo ""
     echo "Updating Mac OS Installation..."
     cd $cloneFolder
-    git pull origin main
+    # git pull origin main
 fi
 
 # Update remote to use SSH
 echo "before cd clone folder"
-cd ${cloneFolder}
+# cd ${cloneFolder}
 echo "after cd clone folder"
-git remote rm origin
-git remote add origin git@github.com:${gitHubUsername}/${gitHubRepository}
-git push --set-upstream origin main
+# git remote rm origin
+# git remote add origin git@github.com:${gitHubUsername}/${gitHubRepository}
+# git push --set-upstream origin main
 echo "to ~"
-cd ~
+# cd ~
