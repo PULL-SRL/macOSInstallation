28fc233e9b453285f600613ecd25f37547c47d09
Mauricio B <m@pull.srl>
2024-08-05 15:33:08 -0400

    install/main:

--- install/main.sh
+++ install/main.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 # Config
-declare gitHubUsername=PULL-SRL
-declare gitHubRepository=macOSInstallation
+# declare gitHubUsername=PULL-SRL
+# declare gitHubRepository=macOSInstallation
 
-# Other configuration (don't modify)
-declare cloneFolder=~/${gitHubRepository}
+# # Other configuration (don't modify)
+# declare cloneFolder=~/${gitHubRepository}
 
 clear
 
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+
 # Repository
-bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) ${gitHubUsername} ${gitHubRepository} ${cloneFolder}
+source <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)
+bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) #${gitHubUsername} ${gitHubRepository} ${cloneFolder}
 cd ${cloneFolder}
+echo ${cloneFolder}
+
 
 # Navigation
-source nav/main.sh # navigation
+# source nav/main.sh # navigation
