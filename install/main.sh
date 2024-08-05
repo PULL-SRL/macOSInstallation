e854603cc1511a777c0738fc868991696bc405cc
Mauricio B <m@pull.srl>
2024-08-05 18:05:46 -0400

    install/main:

--- install/main.sh
+++ install/main.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-# Config
-# declare gitHubUsername=PULL-SRL
-# declare gitHubRepository=macOSInstallation
-
-# # Other configuration (don't modify)
-# declare cloneFolder=~/${gitHubRepository}
-
 clear
 
-echo "#############################"
-echo "#############################"
-echo "#############################"
-echo "####### s f flags ###########"
-echo "#############################"
-echo "#############################"
-
 # Repository
-# source <(curl -s -f https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)# >> ~/test.sh
+source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)" # get config
+bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) ${gitHubUsername} ${gitHubRepository} ${cloneFolder} # get repository
 
-# cd ${cloneFolder}
-source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)";
-echo ${gitHubUsername} ${gitHubRepository} ${cloneFolder}
-echo ">>>>"
-bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) ${gitHubUsername} ${gitHubRepository} ${cloneFolder}
-echo "<<<<"
-
+cd ${clearFolder}
 
 # Navigation
-# source nav/main.sh # navigation
+source nav/main.sh
