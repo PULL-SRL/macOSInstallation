194358ea844736f3a6be8cabab7a7d38049ebde1
Mauricio B <m@pull.srl>
2024-10-09 11:11:13 -0400

    install/main:

--- install/main.sh
+++ install/main.sh
@@ -3,30 +3,10 @@
 clear
 
 # Get repository config
-source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)" # get config
-echo "testing repo config and folder"
-# Get repository
-bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) ${gitHubUsername} ${gitHubRepository} ${cloneFolder} # get repository
+source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/config.sh)"
 
-# Download repository
-if [[ ! -d ${cloneFolder} ]]; then
-    # Clone
-    echo ""
-    echo "Downloading repository... install"
-    git clone https://github.com/${gitHubUsername}/${gitHubRepository}.git ${cloneFolder}
-else
-    # Update
-    echo ""
-    echo "Updating repository...install"
-    cd $cloneFolder
-    git pull origin main
-fi
-
-# Update remote to use SSH
-# cd ${cloneFolder}
-# git remote rm origin
-# git remote add origin git@github.com:${gitHubUsername}/${gitHubRepository}
-# git push --set-upstream origin main
+# Setup repository
+bash <(curl -L https://raw.githubusercontent.com/PULL-SRL/macOSInstallation/main/repository/main.sh) ${gitHubUsername} ${gitHubRepository} ${cloneFolder}
 
 # Start navigation
 cd ${cloneFolder}
