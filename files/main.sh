37006dfd79def25bce8e27c53eb9675c4aa4179b
Mauricio B <m@pull.srl>
2025-11-13 11:31:45 -0400

    files:

--- files/main.sh
+++ files/main.sh
@@ -7,3 +7,7 @@ _i "Installing files..."
 # Dotfiles
 cp files/.aliases ~/.aliases
 echo '\nsource ~/.aliases' >> ~/.zshrc
+
+# Git
+cp files/.gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
